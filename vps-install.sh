49b6513aebe7041603507c5fd9926877e48cb201
akcryptoguy <akcryptoguy+github@gmail.com>
2024-02-21 07:47:33 -0900

    Update VPN script
    This one doesn't break if you already tried it before and will also let you update

--- vps-install.sh
+++ vps-install.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
+cd ~
 echo "fetching latest Hemis version"
 {
 apt-get install unzip -y
-wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-Linux.zip  && unzip Hemis-Linux.zip
-mkdir -p ~/.Hemis-params && cd ~/.Hemis-params && wget https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-params.zip && unzip Hemis-params.zip && cd ..
+.Hemis-cli stop
+rm Hemis*
+rm -rf ~/.Hemis-params
+wget --quiet https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-Linux.zip && unzip Hemis-Linux.zip
+mkdir -p ~/.Hemis-params && cd ~/.Hemis-params && wget https://github.com/Hemis-Blockchain/Hemis/releases/latest/download/Hemis-params.zip && unzip Hemis-params.zip && cd ~
 } &> /dev/null
 echo "Hemis succesfully installed"
 exit
