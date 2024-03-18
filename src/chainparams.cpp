0f837aef883c6b4ccedbe3d89d5402ce20b0f7a5
TheExiledMonk <firewalkerx@gmail.com>
2024-03-18 08:49:11 +0800

    Update chainparams.cpp
    change testnet mining diff

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -430,7 +430,7 @@ public:
 
         consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.fPowNoRetargeting = false;
-        consensus.powLimit   = uint256S("0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.powLimit   = uint256S("0x000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.posLimitV1 = uint256S("0x000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.posLimitV2 = uint256S("0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nBudgetCycleBlocks = 144;         // approx 10 cycles per day
